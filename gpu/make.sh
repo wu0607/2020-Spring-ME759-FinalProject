4d600db755037879bc7d7c63253824ac8c42ca1d
wu0607 <qxyz321@gmail.com>
2020-05-01 11:36:37 -0500

    Start gpu

---
+++ gpu/make.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#SBATCH -p wacc
+#SBATCH --gres=gpu:2 -c 1
+#SBATCH -o make.out
+#SBATCH -e make.err
+# module load cuda
+make
+# ./md5_gpu 923da6e7196b3babf5f95908d145b0bc # abaodog
+./md5_gpu 5501462a4c13dd55a6b236ef4550e3e4 # Erica
