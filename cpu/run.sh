fb4142594152c9a83c9645a74b75cd8b293a4910
Chia-Wei Chen <cchen562@euler.wacc.wisc.edu>
2020-05-03 01:20:16 -0500

    break down next()

--- cpu/run.sh
+++ cpu/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH -p wacc
+#SBATCH --gres=gpu:1 -c 1
 #SBATCH -o run.out
 #SBATCH -e run.err
 
@@ -16,5 +17,6 @@ make
 # abcd with data generate ourself
 # ./md5craker md5 5501462a4c13dd55a6b236ef4550e3e4  # Erica
 # ./md5craker md5 938c2cc0dcc05f2b68c4287040cfcf71 # frog
+echo "hello"
 ./md5craker_omp md5 e2fc714c4727ee9395f324cd2e7f331f   # test abcd using openmp
 mpirun -np 4 ./md5craker_mpi md5 fa246d0262c3925617b0c72bb20eeb1d # test 9999 using mpi
