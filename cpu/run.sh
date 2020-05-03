62f348f26dc0d952a5ce7d3152f52f1304185d36
wu0607 <qxyz321@gmail.com>
2020-05-03 15:59:24 -0500

    add unrolling version

--- cpu/run.sh
+++ cpu/run.sh
@@ -17,6 +17,12 @@ make
 # ./md5craker_omp md5 5501462a4c13dd55a6b236ef4550e3e4  # Erica
 # mpirun -np 2 ./md5craker_mpi md5 5501462a4c13dd55a6b236ef4550e3e4 # test Erica using mpi
 # ./md5craker md5 938c2cc0dcc05f2b68c4287040cfcf71 # frog
-# ./md5craker_omp md5 e2fc714c4727ee9395f324cd2e7f331f   # test abcd using openmp
+echo "===SERIAL==="
+./md5craker md5 e2fc714c4727ee9395f324cd2e7f331f   # test abcd using openmp
+echo -e "\n===PURE OPENMP==="
+./md5craker_omp md5 e2fc714c4727ee9395f324cd2e7f331f   # test abcd using openmp
+echo -e "\n===PURE MPI==="
 mpirun -np 4 ./md5craker_mpi md5 e2fc714c4727ee9395f324cd2e7f331f # test abcd using mpi
+echo -e "\n===MPI + OPENMP==="
+mpirun -np 4 ./md5craker_hybrid md5 e2fc714c4727ee9395f324cd2e7f331f # test abcd using mpi
 # mpirun -np 4 ./md5craker_mpi md5 fa246d0262c3925617b0c72bb20eeb1d # test 9999 using mpi
